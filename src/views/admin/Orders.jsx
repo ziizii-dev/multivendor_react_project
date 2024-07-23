3d6fa9fdf5750dcccde3b01cb08593f119a59a4c
ziizii-dev <ziizii.dev@gmail.com>
2024-07-23 17:02:41 +0630

    master

--- src/views/admin/Orders.jsx
+++ src/views/admin/Orders.jsx
@@ -24,38 +24,39 @@ const Orders = () => {
             placeholder="search"
           />
         </div>
-        <div className='relative mt-5 overflow-x-auto'>
-            <div className='w-full text-sm text-left [#d0d2d6]'>
-                <div className='text-sm text-[#d0d2d6] uppercase border-b border-slate-700'>
-      <div className=" flex justify-between items-center">
-        <div className="py-3 w-[25%] font-bold">Order id</div>
-        <div className="py-3 w-[13%] font-bold">Price</div>
-        <div className="py-3 w-[18%] font-bold">Payment Status</div>
-        <div className="py-3 w-[18%] font-bold">Order Status</div>
-        <div className="py-3 w-[18%] font-bold">Action </div>
-        <div className="py-3 w-[8%] font-bold">
-          <LuArrowDownSquare />{" "}
+        <div className="relative mt-5 overflow-x-auto">
+          <div className="w-full text-sm text-left [#d0d2d6]">
+            <div className="text-sm text-[#d0d2d6] uppercase border-b border-slate-700">
+              <div className=" flex justify-between items-center">
+                <div className="py-3 w-[25%] font-bold">Order id</div>
+                <div className="py-3 w-[13%] font-bold">Price</div>
+                <div className="py-3 w-[18%] font-bold">Payment Status</div>
+                <div className="py-3 w-[18%] font-bold">Order Status</div>
+                <div className="py-3 w-[18%] font-bold">Action </div>
+                <div className="py-3 w-[8%] font-bold">
+                  <LuArrowDownSquare />{" "}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="text-[#d0d2d6] ">
+          <div className=" flex justify-between items-start border-b border-slate-700">
+            <div className="py-3 w-[25%] font-medium whitespace-nowrap">
+              #34343
+            </div>
+            <div className="py-3 w-[13%] font-medium">$654</div>
+            <div className="py-3 w-[18%] font-medium">Pending</div>
+            <div className="py-3 w-[18%] font-medium">Pending</div>
+            <div className="py-3 w-[18%] font-medium">
+              <Link>View</Link>
+            </div>
+            <div className="py-3 w-[8%] font-medium">
+              <LuArrowDownSquare />
+            </div>
+          </div>
         </div>
       </div>
-      </div>
-      </div>
-      </div>
-      <div className='text-[#d0d2d6] '>
-<div className=' flex justify-between items-start border-b border-slate-700'>
-    <div className='py-3 w-[25%] font-medium whitespace-nowrap'>#34343</div>
-    <div className='py-3 w-[13%] font-medium'>$654</div>
-    <div className='py-3 w-[18%] font-medium'>Pending</div>
-    <div className='py-3 w-[18%] font-medium'>Pending</div>
-    <div className='py-3 w-[18%] font-medium'>
-        <Link>View</Link>
-         </div>
-    <div className='py-3 w-[8%] font-medium'><LuArrowDownSquare />
-</div> 
-</div> 
-    </div>
-      </div>
-      
-     
     </div>
   );
 };
