4c24c41b5fd62f18f0529eb16e4b05cd37cf22af
ziizii-dev <ziizii.dev@gmail.com>
2024-07-23 16:55:52 +0630

    master

--- src/router/routes/adminRoutes.jsx
+++ src/router/routes/adminRoutes.jsx
@@ -1,10 +1,17 @@
 // import Home from "../../views/Home";
-import { lazy } from "react";
+
+import { lazy } from "react";    
+const Orders = lazy(()=> import('../../views/admin/Orders.jsx'))   
 const AdminDashboard = lazy(() => import('../../views/admin/AdminDashboard'));
 export const adminRoutes =[
         {
             path:'/admin/dashboard',
             element:<AdminDashboard/>,
             role:'admin'
+        },
+        {
+            path: 'admin/dashboard/orders',
+            element : <Orders/> ,
+            role : 'admin'
         }
 ]
