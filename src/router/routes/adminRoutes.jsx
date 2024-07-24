89dc383877a3098f4d7d95e994645efe2cb5efd3
ziizii-dev <ziizii.dev@gmail.com>
2024-07-24 16:56:15 +0630

    master

--- src/router/routes/adminRoutes.jsx
+++ src/router/routes/adminRoutes.jsx
@@ -3,6 +3,9 @@
 import { lazy } from "react";    
 const Orders = lazy(()=> import('../../views/admin/Orders.jsx'))   
 const AdminDashboard = lazy(() => import('../../views/admin/AdminDashboard'));
+const Category = lazy(()=> import('../../views/admin/Category'))  
+const Sellers = lazy(()=> import('../../views/admin/Sellers'))
+const PaymentRequest = lazy(()=> import('../../views/admin/PaymentRequest'))   
 export const adminRoutes =[
         {
             path:'/admin/dashboard',
@@ -12,6 +15,21 @@ export const adminRoutes =[
         {
             path: 'admin/dashboard/orders',
             element : <Orders/> ,
+            role : 'admin'
+        },
+        {
+            path: 'admin/dashboard/category',
+            element : <Category/> ,
+            role : 'admin'
+        },
+        {
+            path: 'admin/dashboard/sellers',
+            element : <Sellers/> ,
+            role : 'admin'
+        },
+        {
+            path: 'admin/dashboard/payment-request',
+            element : <PaymentRequest/> ,
             role : 'admin'
         }
 ]
--- src/views/admin/Orders.jsx
+++ src/views/admin/Orders.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { LuArrowDownSquare } from "react-icons/lu";
 import { Link } from "react-router-dom";
+import Pagination from '../Pagination';
 const Orders = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [searchValue, setSearchValue] = useState("");
   const [parPage, setParPage] = useState(5);
-
+  const [show, setShow] = useState(false);
   return (
     <div className="px-2 lg:px-7 pt-5">
       <div className="w-full p-4 bg-[#6a5fdf] rounded-md">
@@ -33,9 +34,8 @@ const Orders = () => {
                 <div className="py-3 w-[18%] font-bold">Payment Status</div>
                 <div className="py-3 w-[18%] font-bold">Order Status</div>
                 <div className="py-3 w-[18%] font-bold">Action </div>
-                <div className="py-3 w-[8%] font-bold">
-                  <LuArrowDownSquare />{" "}
-                </div>
+                {/* <div className='py-3 w-[8%] font-medium'><LuArrowDownSquare />
+                </div> */}
               </div>
             </div>
           </div>
@@ -51,12 +51,243 @@ const Orders = () => {
             <div className="py-3 w-[18%] font-medium">
               <Link>View</Link>
             </div>
-            <div className="py-3 w-[8%] font-medium">
+            <div
+              onClick={(e) => setShow(!show)}
+              className="py-3 w-[8%] font-medium"
+            >
               <LuArrowDownSquare />
+            </div>
+          </div>
+          <div
+            className={
+              show ? "block border-b border-slate-700 bg-[#8288ed]" : "hidden"
+            }
+          >
+            <div className=" flex justify-start items-start border-b border-slate-700">
+              <div className="py-3 w-[25%] font-medium whitespace-nowrap pl-3">
+                #3434
+              </div>
+              <div className="py-3 w-[13%] font-medium">$56</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+            </div>
+          </div>
+          <div
+            className={
+              show ? "block border-b border-slate-700 bg-[#8288ed]" : "hidden"
+            }
+          >
+            <div className=" flex justify-start items-start border-b border-slate-700">
+              <div className="py-3 w-[25%] font-medium whitespace-nowrap pl-3">
+                #3434
+              </div>
+              <div className="py-3 w-[13%] font-medium">$56</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+            </div>
+          </div>
+        </div>
+        <div className="text-[#d0d2d6] ">
+          <div className=" flex justify-between items-start border-b border-slate-700">
+            <div className="py-3 w-[25%] font-medium whitespace-nowrap">
+              #34343
+            </div>
+            <div className="py-3 w-[13%] font-medium">$654</div>
+            <div className="py-3 w-[18%] font-medium">Pending</div>
+            <div className="py-3 w-[18%] font-medium">Pending</div>
+            <div className="py-3 w-[18%] font-medium">
+              <Link>View</Link>
+            </div>
+            <div
+              onClick={(e) => setShow(!show)}
+              className="py-3 w-[8%] font-medium"
+            >
+              <LuArrowDownSquare />
+            </div>
+          </div>
+          <div
+            className={
+              show ? "block border-b border-slate-700 bg-[#8288ed]" : "hidden"
+            }
+          >
+            <div className=" flex justify-start items-start border-b border-slate-700">
+              <div className="py-3 w-[25%] font-medium whitespace-nowrap pl-3">
+                #3434
+              </div>
+              <div className="py-3 w-[13%] font-medium">$56</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+            </div>
+          </div>
+          <div
+            className={
+              show ? "block border-b border-slate-700 bg-[#8288ed]" : "hidden"
+            }
+          >
+            <div className=" flex justify-start items-start border-b border-slate-700">
+              <div className="py-3 w-[25%] font-medium whitespace-nowrap pl-3">
+                #3434
+              </div>
+              <div className="py-3 w-[13%] font-medium">$56</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="text-[#d0d2d6] ">
+          <div className=" flex justify-between items-start border-b border-slate-700">
+            <div className="py-3 w-[25%] font-medium whitespace-nowrap">
+              #34343
+            </div>
+            <div className="py-3 w-[13%] font-medium">$654</div>
+            <div className="py-3 w-[18%] font-medium">Pending</div>
+            <div className="py-3 w-[18%] font-medium">Pending</div>
+            <div className="py-3 w-[18%] font-medium">
+              <Link>View</Link>
+            </div>
+            <div
+              onClick={(e) => setShow(!show)}
+              className="py-3 w-[8%] font-medium"
+            >
+              <LuArrowDownSquare />
+            </div>
+          </div>
+          <div
+            className={
+              show ? "block border-b border-slate-700 bg-[#8288ed]" : "hidden"
+            }
+          >
+            <div className=" flex justify-start items-start border-b border-slate-700">
+              <div className="py-3 w-[25%] font-medium whitespace-nowrap pl-3">
+                #3434
+              </div>
+              <div className="py-3 w-[13%] font-medium">$56</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+            </div>
+          </div>
+          <div
+            className={
+              show ? "block border-b border-slate-700 bg-[#8288ed]" : "hidden"
+            }
+          >
+            <div className=" flex justify-start items-start border-b border-slate-700">
+              <div className="py-3 w-[25%] font-medium whitespace-nowrap pl-3">
+                #3434
+              </div>
+              <div className="py-3 w-[13%] font-medium">$56</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+            </div>
+          </div>
+        </div>
+        <div className="text-[#d0d2d6] ">
+          <div className=" flex justify-between items-start border-b border-slate-700">
+            <div className="py-3 w-[25%] font-medium whitespace-nowrap">
+              #34343
+            </div>
+            <div className="py-3 w-[13%] font-medium">$654</div>
+            <div className="py-3 w-[18%] font-medium">Pending</div>
+            <div className="py-3 w-[18%] font-medium">Pending</div>
+            <div className="py-3 w-[18%] font-medium">
+              <Link>View</Link>
+            </div>
+            <div
+              onClick={(e) => setShow(!show)}
+              className="py-3 w-[8%] font-medium"
+            >
+              <LuArrowDownSquare />
+            </div>
+          </div>
+          <div
+            className={
+              show ? "block border-b border-slate-700 bg-[#8288ed]" : "hidden"
+            }
+          >
+            <div className=" flex justify-start items-start border-b border-slate-700">
+              <div className="py-3 w-[25%] font-medium whitespace-nowrap pl-3">
+                #3434
+              </div>
+              <div className="py-3 w-[13%] font-medium">$56</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+            </div>
+          </div>
+          <div
+            className={
+              show ? "block border-b border-slate-700 bg-[#8288ed]" : "hidden"
+            }
+          >
+            <div className=" flex justify-start items-start border-b border-slate-700">
+              <div className="py-3 w-[25%] font-medium whitespace-nowrap pl-3">
+                #3434
+              </div>
+              <div className="py-3 w-[13%] font-medium">$56</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+            </div>
+          </div>
+        </div>
+        <div className="text-[#d0d2d6] ">
+          <div className=" flex justify-between items-start border-b border-slate-700">
+            <div className="py-3 w-[25%] font-medium whitespace-nowrap">
+              #34343
+            </div>
+            <div className="py-3 w-[13%] font-medium">$654</div>
+            <div className="py-3 w-[18%] font-medium">Pending</div>
+            <div className="py-3 w-[18%] font-medium">Pending</div>
+            <div className="py-3 w-[18%] font-medium">
+              <Link>View</Link>
+            </div>
+            <div
+              onClick={(e) => setShow(!show)}
+              className="py-3 w-[8%] font-medium"
+            >
+              <LuArrowDownSquare />
+            </div>
+          </div>
+          <div
+            className={
+              show ? "block border-b border-slate-700 bg-[#8288ed]" : "hidden"
+            }
+          >
+            <div className=" flex justify-start items-start border-b border-slate-700">
+              <div className="py-3 w-[25%] font-medium whitespace-nowrap pl-3">
+                #3434
+              </div>
+              <div className="py-3 w-[13%] font-medium">$56</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+            </div>
+          </div>
+          <div
+            className={
+              show ? "block border-b border-slate-700 bg-[#8288ed]" : "hidden"
+            }
+          >
+            <div className=" flex justify-start items-start border-b border-slate-700">
+              <div className="py-3 w-[25%] font-medium whitespace-nowrap pl-3">
+                #3434
+              </div>
+              <div className="py-3 w-[13%] font-medium">$56</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
             </div>
           </div>
         </div>
       </div>
+      <div className='w-full flex justify-end mt-4 bottom-4 right-4'>
+      <Pagination 
+            pageNumber = {currentPage}
+            setPageNumber = {setCurrentPage}
+            totalItem = {50}
+            parPage = {parPage}
+            showItem = {3}
+        />
+      </div>
+     
+
     </div>
   );
 };
