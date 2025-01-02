aa944bbf102f930898c23277260e1ca7eb9294e5
ziizii-dev <ziizii.dev@gmail.com>
2025-01-02 14:38:09 +0630

    master

--- src/views/admin/ChatSeller.jsx
+++ src/views/admin/ChatSeller.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { IoMdClose } from "react-icons/io";
 const ChatSeller = () => {
     const [show, setShow] = useState(false) 
-    
     return (
     <div className='px-2 lg:px-7 py-5'>
         <div className='w-full bg-[#6a5fdf] px-4 py-4 rounded-md h-[calc(100vh-140px)]'>
