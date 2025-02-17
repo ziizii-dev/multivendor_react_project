bf3f35059605c3d4c1dae428c5da04d68f31a7b9
ziizii-dev <ziizii.dev@gmail.com>
2025-02-17 23:31:53 +0630

    master

--- src/views/admin/ChatSeller.jsx
+++ src/views/admin/ChatSeller.jsx
@@ -118,12 +118,14 @@ const ChatSeller = () => {
 
             </div> 
         </div>
+        
         <form className='flex gap-3'>
             <input className='w-full flex justify-between px-2 border border-slate-700 items-center py-[5px] focus:border-blue-500 rounded-md outline-none bg-transparent text-[#d0d2d6]' type="text" placeholder='Input Your Message' />
             <button className='shadow-lg bg-[#06b6d4] hover:shadow-cyan-500/50 text-semibold w-[75px] h-[35px] rounded-md text-white flex justify-center items-center'>Send</button>
 
         </form>
-        
+
+
 
     </div>
 
