8ca5155c3c8e3a3f6aab0b394b42480cc0f14c6c
ziizii-dev <ziizii.dev@gmail.com>
2025-05-28 18:56:05 +0630

    master

--- src/router/routes/adminRoutes.jsx
+++ src/router/routes/adminRoutes.jsx
@@ -53,6 +53,7 @@ export const adminRoutes =[
         },
         {
             
+
             path: 'admin/dashboard/chat-sellers',
             element : <ChatSeller/> ,
             role : 'admin'
