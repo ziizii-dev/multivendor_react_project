fc483779a2e3edf0896b81f2e6c8db7c653a6250
ziizii-dev <ziizii.dev@gmail.com>
2025-07-02 20:31:24 +0630

    master

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -9,14 +9,13 @@ const MainLayout = () => {
   return (
       <>
 
-
       <div className='bg-[#cdcae9] w-[full] min-h-screen '>
       <Header showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
       <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
       <div className='ml-0 lg:ml-[260px] pt-[95px] transition-all'>
         <Outlet/>
       </div>
-      
+
 
       </div>
 
