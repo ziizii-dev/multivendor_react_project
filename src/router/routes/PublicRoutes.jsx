2150e48e45117dfe657f648b11750211337bbbaf
ziizii-dev <ziizii.dev@gmail.com>
2025-07-13 15:45:37 +0630

    master

--- src/router/routes/PublicRoutes.jsx
+++ src/router/routes/PublicRoutes.jsx
@@ -7,6 +7,7 @@ const PublicRoutes = [
     path: '/login',
     element: <Login />
   },
+  
   {
     path: '/register',
     element: <Register />
@@ -16,7 +17,7 @@ const PublicRoutes = [
     path: '/admin/login',
     element: <AdminLogin />
   }
-  
+
 ];
 
 export default PublicRoutes;
