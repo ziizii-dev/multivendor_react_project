951a038f5ce42ce946850c0fd86dd033a8808d12
ziizii-dev <ziizii.dev@gmail.com>
2025-07-15 19:56:36 +0630

    master

--- src/views/admin/ChatSeller.jsx
+++ src/views/admin/ChatSeller.jsx
@@ -5,6 +5,7 @@ const ChatSeller = () => {
     const [show, setShow] = useState(false) 
     const sellerId = 65
   return (
+    
     <div className="px-2 lg:px-7 py-5">
       <div className="w-full bg-[#6a5fdf] px-4 py-4 rounded-md h-[calc(100vh-140px)]">
       <div className='flex w-full h-full relative'>
