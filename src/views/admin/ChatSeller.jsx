50350dbc755fc9c91742f279f7b5470466006ba8
ziizii-dev <ziizii.dev@gmail.com>
2025-07-16 20:06:14 +0630

    master

--- src/views/admin/ChatSeller.jsx
+++ src/views/admin/ChatSeller.jsx
@@ -5,7 +5,7 @@ const ChatSeller = () => {
     const [show, setShow] = useState(false) 
     const sellerId = 65
   return (
-    
+
     <div className="px-2 lg:px-7 py-5">
       <div className="w-full bg-[#6a5fdf] px-4 py-4 rounded-md h-[calc(100vh-140px)]">
       <div className='flex w-full h-full relative'>
@@ -29,6 +29,11 @@ const ChatSeller = () => {
                     <h2 className="text-base font-semibold">Earth</h2>
                   </div>
                 </div>
+
+
+
+
+                
               </div>
               <div
                 className={`h-[60px] flex justify-start gap-2 items-center text-white px-2 py-2 rounded-sm cursor-pointer`}
