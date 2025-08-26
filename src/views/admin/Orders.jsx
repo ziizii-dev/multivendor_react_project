80c7e8435fbb606f60179c7775262cf771b8c306
ziizii-dev <ziizii.dev@gmail.com>
2025-08-26 19:04:51 +0630

    master

--- src/views/admin/Orders.jsx
+++ src/views/admin/Orders.jsx
@@ -20,6 +20,7 @@ const Orders = () => {
             <option value="20">20</option>
           </select>
           
+
           <input
             className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
             type="text"
