c7dfbbebcf50b76c4e7089585307ad0bdf4fb42e
ziizii-dev <ziizii.dev@gmail.com>
2025-08-31 22:22:19 +0630

    master

--- src/views/admin/Orders.jsx
+++ src/views/admin/Orders.jsx
@@ -22,11 +22,9 @@ const Orders = () => {
           <input
             className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
             type="text"
+            
             placeholder="search"
           />
-
-
-          
         </div>
         <div className="relative mt-5 overflow-x-auto">
           <div className="w-full text-sm text-left [#d0d2d6]">
