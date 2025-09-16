7d12eabc15b0794858a20bd2a841f3ac71b6ed48
ziizii-dev <ziizii.dev@gmail.com>
2025-09-16 21:14:01 +0630

    master

--- src/views/admin/Orders.jsx
+++ src/views/admin/Orders.jsx
@@ -15,9 +15,6 @@ const Orders = () => {
             onChange={(e) => setParPage(parseInt(e.target.value))}
             className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
           >
-
-            
-            
             <option value="5">5</option>
             <option value="10">10</option>
             <option value="20">20</option>
