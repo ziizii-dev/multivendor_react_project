1a84427da409675a171ba807d88431c1f404e9dc
ziizii-dev <ziizii.dev@gmail.com>
2025-10-03 21:44:31 +0630

    master

--- src/views/admin/Orders.jsx
+++ src/views/admin/Orders.jsx
@@ -21,6 +21,7 @@ const Orders = () => {
           </select>
 
 
+
           <input
             className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
             type="text"
