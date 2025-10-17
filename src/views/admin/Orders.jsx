f5d6fa5d0e730819ef2ace38185910205938ba49
ziizii-dev <ziizii.dev@gmail.com>
2025-10-18 00:48:31 +0630

    master

--- src/views/admin/Orders.jsx
+++ src/views/admin/Orders.jsx
@@ -12,6 +12,7 @@ const Orders = () => {
       <div className="w-full p-4 bg-[#6a5fdf] rounded-md">
         <div className="flex justify-between items-center">
           <select
+          
             onChange={(e) => setParPage(parseInt(e.target.value))}
             className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
           >
