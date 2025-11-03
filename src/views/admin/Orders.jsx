bd976b015e8bcfbbf273d160fa42e70f366b72ff
ziizii-dev <ziizii.dev@gmail.com>
2025-11-03 22:32:53 +0630

    master

--- src/views/admin/Orders.jsx
+++ src/views/admin/Orders.jsx
@@ -19,6 +19,8 @@ const Orders = () => {
             <option value="5">5</option>
             <option value="10">10</option>
 
+            
+
             <option value="20">20</option>
           </select>
           <input
